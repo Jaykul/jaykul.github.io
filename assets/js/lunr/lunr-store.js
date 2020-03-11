4d1ae34fe638f3d69e6f113b546c0d1ad0bde0d7
GitHub Action <action@github.com>
2020-03-11 05:09:28 +0000

    New changes

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "About Huddled Masses",
+        "excerpt":"This is web site is dedicated to the musings of Joel Bennett (aka Jaykul) about technology, software, software development, the web, and the world. Any resemblance of the views expressed and the views of my employer, my terminal, or the view out my window are purely coincidental. The resemblance between...","categories": [],
+        "tags": ["site-news"],
+        "url": "https://huddledmasses.org/2014/07/about-huddled-masses/",
+        "teaser": null
+      },{
+        "title": "Missing Content",
+        "excerpt":"Since I’ve blown away all my old blog content, there are a lot of pages missing that were really useful. I’m going to bring the best of them back, and I have plans to actually rewrite some of the older ones to update them (part of the reason I let...","categories": [],
+        "tags": ["site-news","missing-content"],
+        "url": "https://huddledmasses.org/2014/07/missing-content/",
+        "teaser": null
+      },{
+        "title": "A Fresh Start",
+        "excerpt":"A few weeks ago, someone somehow injected a shell onto my WordPress powered blog site. They managed to get multiple hacking tools installed, delete files required for hosting, and who knows what else. Needless to say I was frustrated and upset. In fact, I was so frustrated that to get...","categories": [],
+        "tags": ["site-news"],
+        "url": "https://huddledmasses.org/2014/07/a-fresh-start/",
+        "teaser": null
+      },{
+        "title": "Validating Self-Signed Certificates From .Net and PowerShell",
+        "excerpt":"In this article I’m going to present a module that helps you deal with one of the common problems for Windows PowerShell users (and even .Net developers) who are trying to interact from command-line applications with web interfaces (especially those that are hosted internally): Self-signed certificates, and how to ignore...","categories": [],
+        "tags": ["powershell","ssl","rest","webrequest"],
+        "url": "https://huddledmasses.org/2014/07/validating-self-signed-certificates-properly-from-powershell/",
+        "teaser": null
+      },{
+        "title": "How the developer's brain works",
+        "excerpt":"I need to finish the PoshCode module, so I start working on that … I realize the “Configuration” module that is inside PoshCode, is actually more important than PoshCode (that is, I think more developers will use it, because even PowerShellGet users need this) I decide to separate the Configuration...","categories": [],
+        "tags": ["development","personal"],
+        "url": "https://huddledmasses.org/2014/10/developer-brain/",
+        "teaser": null
+      },{
+        "title": "PowerShell Casting Oddities",
+        "excerpt":"PowerShell’s casting can result in some weird results. Before we begin, check out this C# class: using System; public class User { // The user's id (e.g.: StackOverflow id, employee number, or whatever) public UInt32 Id { get; set; } // The user's name (for display). First and last, nickname,...","categories": ["powershell"],
+        "tags": ["powershell","classes","casting"],
+        "url": "https://huddledmasses.org/2020/03/powershell-casting-oddities/",
+        "teaser": null
+      }]
